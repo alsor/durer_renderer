59c4a07cc91f7ab43b7aa1574c554bc8fdf388a6
Alexey Sorokin <alsor.net@gmail.com>
2018-02-12 23:55:52 +0300

    make structs Copy types

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,36 @@ use image::ColorType;
 use image::png::PNGEncoder;
 use std::fs::File;
 
+#[derive(Copy, Clone)]
 struct Point3D { x: f64, y: f64, z: f64 }
 
+#[derive(Copy, Clone)]
 struct Point2D { x: f64, y: f64 }
 
+#[derive(Copy, Clone)]
 struct Frame { x_min: f64, x_max: f64, y_min: f64, y_max: f64 }
 
+#[derive(Copy, Clone)]
 struct Pixel { x: usize, y: usize }
 
-fn project(point3d: &Point3D) -> Point2D {
+fn project(point3d: Point3D) -> Point2D {
     Point2D { x: point3d.x / point3d.z, y: point3d.y / point3d.z }
 }
 
-fn normalize(point2d: &Point2D, frame: &Frame) -> Point2D {
+fn normalize(point2d: Point2D, frame: Frame) -> Point2D {
     Point2D {
         x: 1.0 - (point2d.x - frame.x_min) / (frame.x_max - frame.x_min),
         y: (point2d.y - frame.y_min) / (frame.y_max - frame.y_min)
     }
 }
 
-fn rasterize(point: &Point2D, size: usize) -> Pixel {
+fn rasterize(point: Point2D, size: usize) -> Pixel {
     let x = (size as f64 * point.x) as usize;
     let y = (size as f64 * point.y) as usize;
     Pixel { x, y }
 }
 
-fn rasterize_line(start: &Pixel, end: &Pixel, buffer: &mut [u8], size: usize) {
+fn rasterize_line(start: Pixel, end: Pixel, buffer: &mut [u8], size: usize) {
     let x1 = start.x as i32;
     let y1 = start.y as i32;
     let x2 = end.x as i32;
@@ -65,7 +69,7 @@ fn rasterize_line(start: &Pixel, end: &Pixel, buffer: &mut [u8], size: usize) {
                 d = d + d1;
             }
 
-            render(&Pixel { x: x as usize, y: y as usize}, buffer, size);
+            render(Pixel { x: x as usize, y: y as usize}, buffer, size);
 
             x = x + sx;
         }
@@ -86,7 +90,7 @@ fn rasterize_line(start: &Pixel, end: &Pixel, buffer: &mut [u8], size: usize) {
                 d = d + d1;
             }
 
-            render(&Pixel { x: x as usize, y: y as usize }, buffer, size);
+            render(Pixel { x: x as usize, y: y as usize }, buffer, size);
 
             y = y + sy;
         }
@@ -94,7 +98,7 @@ fn rasterize_line(start: &Pixel, end: &Pixel, buffer: &mut [u8], size: usize) {
 
 }
 
-fn render(pixel: &Pixel, buffer: &mut [u8], size: usize) {
+fn render(pixel: Pixel, buffer: &mut [u8], size: usize) {
     buffer[pixel.x * size + pixel.y] = 255;
 }
 
@@ -141,13 +145,13 @@ fn main() {
     let mut vertex_pixels = Vec::new();
 
     for point3d in &vertices {
-        let point2d = project(point3d);
-        let norm_point = normalize(&point2d, &frame);
-        vertex_pixels.push(rasterize(&norm_point, size));
+        let point2d = project(*point3d);
+        let norm_point = normalize(point2d, frame);
+        vertex_pixels.push(rasterize(norm_point, size));
     }
 
     for edge in &edges {
-        rasterize_line(&vertex_pixels[edge.0], &vertex_pixels[edge.1], &mut buffer, size);
+        rasterize_line(vertex_pixels[edge.0], vertex_pixels[edge.1], &mut buffer, size);
     }
 
     write_image(&buffer, size).expect("Error writing image to file");
